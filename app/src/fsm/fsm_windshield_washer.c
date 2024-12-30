f01d94c441e70168664dd5c55de47c632104777c
rolln7drktayau <roland_cedric_tayo@hotmail.com>
2024-12-30 11:13:27 +0100

    add FSM implementation for windshield washer and wiper control

---
+++ app/src/fsm/fsm_windshield_washer.c
@@ -0,0 +1,226 @@
+/**
+ * \file        fsm_windshield_washer.c
+ * \brief       Finite State Machine for windshield washer and wiper control
+ * \details     Implementation of the FSM controlling wipers and washer states
+ * \author Raphael CAUSSE - Melvyn MUNOZ - Roland Cedric TAYO
+ */
+
+#include "fsm_windshield_washer.h"
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+/* Static variables */
+static uint32_t timer_counter = 0;
+static const uint32_t TIMER_2SEC = 20; // 20 * 100ms = 2 seconds
+
+/* Debug string arrays for states and events */
+static const char *STATE_NAMES[] = {
+    [ST_ALL_OFF] = "ALL_OFF",
+    [ST_WIPER_ON] = "WIPER_ON",
+    [ST_WASHER_AND_WIPER_ON] = "WASHER_AND_WIPER_ON",
+    [ST_WIPER_TIMER] = "WIPER_TIMER"};
+
+static const char *EVENT_NAMES[] = {
+    [EV_NONE] = "NONE",
+    [EV_CMD_WIPER] = "CMD_WIPER",
+    [EV_CMD_WASHER] = "CMD_WASHER",
+    [EV_TIMER_2SEC] = "TIMER_2SEC"};
+
+/* Callback functions for state transitions */
+static int handle_all_off(void)
+{
+    printf("DEBUG: FSM - Executing ALL_OFF actions\n");
+    set_cmd_wiper(WIPER_OFF);
+    set_cmd_washer(WASHER_OFF);
+    timer_counter = 0;
+    return 0;
+}
+
+static int handle_wiper_on(void)
+{
+    printf("DEBUG: FSM - Executing WIPER_ON actions\n");
+    set_cmd_wiper(WIPER_LOW);
+    set_cmd_washer(WASHER_OFF);
+    timer_counter = 0;
+    return 0;
+}
+
+static int handle_washer_and_wiper_on(void)
+{
+    printf("DEBUG: FSM - Executing WASHER_AND_WIPER_ON actions\n");
+    set_cmd_wiper(WIPER_LOW);
+    set_cmd_washer(WASHER_ON);
+    timer_counter = 0;
+    return 0;
+}
+
+static int handle_wiper_timer(void)
+{
+    printf("DEBUG: FSM - Executing WIPER_TIMER actions (counter: %lu)\n", (unsigned long)timer_counter);
+    set_cmd_wiper(WIPER_LOW);
+    set_cmd_washer(WASHER_OFF);
+    timer_counter++;
+    return 0;
+}
+
+static int handle_error(void)
+{
+    printf("ERROR: FSM - Error condition triggered\n");
+    set_cmd_wiper(WIPER_OFF);
+    set_cmd_washer(WASHER_OFF);
+    timer_counter = 0;
+    return -1;
+}
+
+/* Transition structure */
+typedef struct
+{
+    fsm_state_t state;
+    fsm_event_t event;
+    int (*callback)(void);
+    fsm_state_t next_state;
+} tTransition;
+
+/* Transition table */
+static tTransition trans[] = {
+    {ST_ALL_OFF, EV_CMD_WIPER, &handle_wiper_on, ST_WIPER_ON},
+    {ST_ALL_OFF, EV_CMD_WASHER, &handle_washer_and_wiper_on, ST_WASHER_AND_WIPER_ON},
+    {ST_WIPER_ON, EV_CMD_WIPER, &handle_all_off, ST_ALL_OFF},
+    {ST_WIPER_ON, EV_CMD_WASHER, &handle_washer_and_wiper_on, ST_WASHER_AND_WIPER_ON},
+    {ST_WASHER_AND_WIPER_ON, EV_CMD_WASHER, &handle_wiper_timer, ST_WIPER_TIMER},
+    {ST_WIPER_TIMER, EV_TIMER_2SEC, &handle_all_off, ST_ALL_OFF},
+    {ST_WIPER_TIMER, EV_CMD_WIPER, &handle_wiper_on, ST_WIPER_ON},
+    {ST_WIPER_TIMER, EV_CMD_WASHER, &handle_washer_and_wiper_on, ST_WASHER_AND_WIPER_ON},
+    {ST_ANY, EV_ERR, &handle_error, ST_ALL_OFF}};
+
+#define TRANS_COUNT (sizeof(trans) / sizeof(*trans))
+
+/* Static variables for FSM state */
+static fsm_state_t current_state = ST_ALL_OFF;
+static cmd_wiper_t last_wiper_cmd = WIPER_OFF;
+static cmd_washer_t last_washer_cmd = WASHER_OFF;
+
+const char *fsm_get_state_name(fsm_state_t state)
+{
+    if (state == ST_ANY)
+        return "ANY";
+    if (state == ST_TERM)
+        return "TERM";
+    if (state >= 0 && state <= ST_WIPER_TIMER)
+        return STATE_NAMES[state];
+    return "UNKNOWN";
+}
+
+const char *fsm_get_event_name(fsm_event_t event)
+{
+    if (event == EV_ANY)
+        return "ANY";
+    if (event == EV_ERR)
+        return "ERROR";
+    if (event >= 0 && event <= EV_TIMER_2SEC)
+        return EVENT_NAMES[event];
+    return "UNKNOWN";
+}
+
+static fsm_event_t get_next_event(fsm_state_t current_state)
+{
+    fsm_event_t event = EV_NONE;
+
+    cmd_wiper_t current_wiper_cmd = get_cmd_wiper();
+    cmd_washer_t current_washer_cmd = get_cmd_washer();
+
+    if (current_wiper_cmd != last_wiper_cmd)
+    {
+        printf("DEBUG: FSM - Wiper command changed: %d -> %d\n", last_wiper_cmd, current_wiper_cmd);
+        last_wiper_cmd = current_wiper_cmd;
+        if (current_wiper_cmd != WIPER_OFF)
+        {
+            event = EV_CMD_WIPER;
+        }
+    }
+    else if (current_washer_cmd != last_washer_cmd)
+    {
+        printf("DEBUG: FSM - Washer command changed: %d -> %d\n", last_washer_cmd, current_washer_cmd);
+        last_washer_cmd = current_washer_cmd;
+        if (current_washer_cmd != WASHER_OFF)
+        {
+            event = EV_CMD_WASHER;
+        }
+    }
+
+    if (current_state == ST_WIPER_TIMER && timer_counter >= TIMER_2SEC)
+    {
+        printf("DEBUG: FSM - 2-second timer expired\n");
+        event = EV_TIMER_2SEC;
+    }
+
+    return event;
+}
+
+void fsm_windshield_washer_init(void)
+{
+    printf("INFO: FSM - Initializing windshield washer FSM\n");
+    current_state = ST_ALL_OFF;
+    last_wiper_cmd = WIPER_OFF;
+    last_washer_cmd = WASHER_OFF;
+    timer_counter = 0;
+    handle_all_off();
+}
+
+int fsm_windshield_washer_run(void)
+{
+    int i;
+    int ret = 0;
+    fsm_event_t event = EV_NONE;
+    fsm_state_t state = ST_ALL_OFF;
+
+    /* While FSM hasn't reached end state */
+    while (state != ST_TERM)
+    {
+        /* Get event */
+        event = get_next_event(state);
+
+        if (event != EV_NONE)
+        {
+            printf("DEBUG: FSM - State: %s, Event: %s\n",
+                   fsm_get_state_name(state),
+                   fsm_get_event_name(event));
+        }
+
+        /* For each transition */
+        for (i = 0; i < TRANS_COUNT; i++)
+        {
+            /* If State is current state OR The transition applies to all states */
+            if ((state == trans[i].state) || (ST_ANY == trans[i].state))
+            {
+                /* If event is the transition event OR the event applies to all */
+                if ((event == trans[i].event) || (EV_ANY == trans[i].event))
+                {
+                    /* Record the transition for debugging */
+                    if (event != EV_NONE)
+                    {
+                        printf("INFO: FSM - Transition: %s -> %s\n",
+                               fsm_get_state_name(state),
+                               fsm_get_state_name(trans[i].next_state));
+                    }
+
+                    /* Apply the new state */
+                    state = trans[i].next_state;
+                    if (trans[i].callback != NULL)
+                    {
+                        /* Call the state function */
+                        ret = (trans[i].callback)();
+                        if (ret != 0)
+                        {
+                            return ret;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    return ret;
+}
---
+++ app/src/fsm/fsm_windshield_washer.h
@@ -0,0 +1,83 @@
+/**
+ * \file        fsm_windshield_washer.h
+ * \brief       Finite State Machine for windshield washer and wiper control
+ * \details     Header file containing FSM states, events and function declarations
+ * \author Raphael CAUSSE - Melvyn MUNOZ - Roland Cedric TAYO
+ */
+
+#ifndef FSM_WINDSHIELD_WASHER_H
+#define FSM_WINDSHIELD_WASHER_H
+
+#include "bcgv_api.h"
+#include <stdbool.h>
+
+/* Command types definitions */
+typedef enum
+{
+    WIPER_OFF = 0,
+    WIPER_LOW = 1,
+    WIPER_HIGH = 2
+} cmd_wiper_t;
+
+typedef enum
+{
+    WASHER_OFF = 0,
+    WASHER_ON = 1
+} cmd_washer_t;
+
+/* States for debug prints */
+typedef enum
+{
+    ST_ANY = -1,                /* Any state */
+    ST_ALL_OFF = 0,             /* Initial state: everything off */
+    ST_WIPER_ON = 1,            /* Only wipers active */
+    ST_WASHER_AND_WIPER_ON = 2, /* Both washer and wipers active */
+    ST_WIPER_TIMER = 3,         /* Wipers active with timer */
+    ST_TERM = 255               /* Terminal state (not used) */
+} fsm_state_t;
+
+/* Events for debug prints */
+typedef enum
+{
+    EV_ANY = -1,       /* Any event */
+    EV_NONE = 0,       /* No event */
+    EV_CMD_WIPER = 1,  /* Wiper command change */
+    EV_CMD_WASHER = 2, /* Washer command change */
+    EV_TIMER_2SEC = 3, /* 2-second timer expired */
+    EV_ERR = 255       /* Error event */
+} fsm_event_t;
+
+/* Function prototypes for command getters/setters */
+cmd_wiper_t get_cmd_wiper(void);
+void set_cmd_wiper(cmd_wiper_t cmd);
+cmd_washer_t get_cmd_washer(void);
+void set_cmd_washer(cmd_washer_t cmd);
+
+/**
+ * \brief Initialize the windshield washer FSM
+ * \details Must be called once before using the FSM
+ */
+void fsm_windshield_washer_init(void);
+
+/**
+ * \brief Run the windshield washer FSM
+ * \details Main FSM execution function
+ * \return 0 on success, non-zero on error
+ */
+int fsm_windshield_washer_run(void);
+
+/**
+ * \brief Get current state name for debug prints
+ * \param state State enum value
+ * \return String representation of the state
+ */
+const char *fsm_get_state_name(fsm_state_t state);
+
+/**
+ * \brief Get event name for debug prints
+ * \param event Event enum value
+ * \return String representation of the event
+ */
+const char *fsm_get_event_name(fsm_event_t event);
+
+#endif // FSM_WINDSHIELD_WASHER_H
