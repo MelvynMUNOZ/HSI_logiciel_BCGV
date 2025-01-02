969f16bdbea1b52493ab36ba02794f0d283b560d
Raphael CAUSSE <raphael.causse2@gmail.com>
2025-01-02 15:22:26 +0100

    fix: app main + fsm template
    Tmp base for app main that compiles without error
    Remove fsm template source file as it is not needed anymore

--- app/src/app.c
+++ app/src/app.c
@@ -7,18 +7,14 @@
 
 #include "drv_api.h"
 #include "bcgv_api.h"
-#include "comodo.h"
 
 int main(void)
 {
     int32_t ret;
     int32_t driver_fd;
     uint8_t udp_frame[DRV_UDP_100MS_FRAME_SIZE];
-    uint8_t comodo_frame[COMODO_SERIAL_FRAME_SIZE];
 
     /***** Starting application *****/
-
-    context_init(); // Initialize the context
 
     driver_fd = drv_open();
     if (driver_fd == DRV_ERROR)
@@ -31,8 +27,9 @@ int main(void)
         printf("ERROR: driver version mismatch\n");
         return EXIT_FAILURE;
     }
-
     printf("INFO: driver opened\n");
+
+    bcgv_ctx_init();
 
     /***** Main loop *****/
 
@@ -45,25 +42,15 @@ int main(void)
             continue;
         }
 
+        /* TMP: print each byte for now */
         printf("MUX > [");
         for (size_t i = 0; i < DRV_UDP_100MS_FRAME_SIZE; i++)
         {
-            printf("%c", udp_frame[i]); // Print each byte as a character for now
+            printf("%02X ", udp_frame[i]);
         }
         printf("]\n");
 
-        // Assuming the COMODO frame is part of the UDP frame, extract and decode it
-        comodo_frame[0] = udp_frame[0]; // Example extraction logic
-        if (!comodo_decode(comodo_frame))
-        {
-            printf("ERROR: failed to decode COMODO frame\n");
-        }
-
-        // Decode the rest of the UDP frame and update application data
-        if (!mux_decode(udp_frame))
-        {
-            printf("ERROR: failed to decode MUX frame\n");
-        }
+        /* TODO: real implementation */
     }
 
     /***** Closing application *****/
@@ -74,7 +61,6 @@ int main(void)
         printf("ERROR: failed to close driver\n");
         return EXIT_FAILURE;
     }
-
     printf("INFO: driver closed\n");
 
     return EXIT_SUCCESS;
