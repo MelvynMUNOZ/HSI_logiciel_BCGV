43b8cf1a4c3a91e769095e7abdc599a70aa7ce37
Raphael CAUSSE <raphael.causse2@gmail.com>
2025-01-08 23:56:24 +0100

    refactor: mux
    Refactor MUX code into functions
    Update app main to fit architecture requirements

--- app/src/app.c
+++ app/src/app.c
@@ -8,51 +8,58 @@
 #include "drv_api.h"
 #include "bcgv_api.h"
 #include "mux.h"
+#include "log.h"
 
 int main(void)
 {
-    int32_t ret;
-    int32_t driver_fd;
-    uint8_t udp_frame[DRV_UDP_100MS_FRAME_SIZE];
+    bool quit = false;
+    bool success = false;
+    int32_t ret = 0;
+    int32_t driver_fd = 0;
+    uint8_t mux_frame[DRV_UDP_100MS_FRAME_SIZE] = {0};
 
     /***** Starting application *****/
 
     driver_fd = drv_open();
     if (driver_fd == DRV_ERROR)
     {
-        printf("ERROR: failed to open driver\n");
+        log_error("failed to open driver", NULL);
         return EXIT_FAILURE;
     }
     else if (driver_fd == DRV_VER_MISMATCH)
     {
-        printf("ERROR: driver version mismatch\n");
+        log_error("driver version mismatch", NULL);
         return EXIT_FAILURE;
     }
-    printf("INFO: driver opened\n");
+    log_info("driver opened", NULL);
 
     bcgv_ctx_init();
 
     /***** Main loop *****/
 
-    while (1)
+    while (quit == false)
     {
-        ret = drv_read_udp_100ms(driver_fd, udp_frame);
-        if (ret == DRV_ERROR)
+        /* Receive and decode frame from MUX */
+        success = mux_read_frame_100ms(driver_fd, mux_frame);
+        if (success == false)
         {
-            printf("ERROR: failed to read udp frame 100ms\n");
+            continue; /* Behaviour to define: should we exit ? continue to next frame forever */
+        }
+        mux_check_frame_number(mux_frame);
+        success = mux_decode_frame(mux_frame);
+        if (success == false)
+        {
             continue;
         }
 
-        /* TMP: print each byte for now */
-        printf("\nMUX > [ ");
-        for (size_t i = 0; i < DRV_UDP_100MS_FRAME_SIZE; i++)
-        {
-            printf("%02X ", udp_frame[i]);
-        }
-        printf("]\n");
-        /* END TMP */
+        /* DEBUG */
+        printf("====================");
+        mux_print_raw(mux_frame);
+        mux_print_decoded();
+        printf("====================");
+        /* END DEBUG*/
 
-        mux_decode(udp_frame);
+        /* TODO: other implementations */
     }
 
     /***** Closing application *****/
@@ -60,10 +67,10 @@ int main(void)
     ret = drv_close(driver_fd);
     if (ret == DRV_ERROR)
     {
-        printf("ERROR: failed to close driver\n");
+        log_error("failed to close driver", NULL);
         return EXIT_FAILURE;
     }
-    printf("INFO: driver closed\n");
+    log_info("driver closed", NULL);
 
     return EXIT_SUCCESS;
 }
--- app/src/mux.h
+++ app/src/mux.h
@@ -11,10 +11,47 @@
 #include "drv_api.h"
 
 /**
- * \brief Decode UDP frame from MUX and update application data
- * \param frame : Pointer to the UDP frame buffer
- * \return bool : true if the frame was successfully decoded, false otherwise
+ * \brief Read UDP 100ms frame (blocking call).
+ * \details Wait for next UDP 100ms frame and returns it.
+ * \param drv_fd : File descriptor of channel with driver (get with open)
+ * \param frame : Pointer to the MUX frame buffer
+ * \return bool :
  */
-bool mux_decode(const uint8_t frame[DRV_UDP_100MS_FRAME_SIZE]);
+bool mux_read_frame_100ms(int32_t drv_fd, uint8_t frame[DRV_UDP_100MS_FRAME_SIZE]);
+
+/**
+ * \brief Check for a valid frame number in the frame.
+ * \details If there the frame number is not equal to the expected frame number (incremental and looping at 100), a warning is logged.
+ * \param frame : Pointer to the MUX frame buffer
+ */
+void mux_check_frame_number(const uint8_t frame[DRV_UDP_100MS_FRAME_SIZE]);
+
+/**
+ * \brief Decode UDP frame from MUX and update application data.
+ * \details Frame format (15 bytes, Big endian):
+ *          - 1 byte (MSB): Frame number (1-100)
+ *          - 4 bytes : Distance (km)
+ *          - 1 byte : Speed (km/h)
+ *          - 1 byte : Chassis issues
+ *          - 1 byte : Motor issues
+ *          - 1 byte : Fuel level (litres)
+ *          - 4 bytes: Engine RPM
+ *          - 1 byte (LSB): Battery issues
+ *          - 1 byte : CRC8
+ * \param frame : Pointer to the MUX frame buffer
+ * \return bool : true if the frame crc8 is valid and frame has been decoded, false otherwise
+ */
+bool mux_decode_frame(const uint8_t frame[DRV_UDP_100MS_FRAME_SIZE]);
+
+/**
+ * \brief Print raw bytes of MUX frame.
+ * \param frame : Pointer to the MUX frame buffer
+ */
+void mux_print_raw(const uint8_t frame[DRV_UDP_100MS_FRAME_SIZE]);
+
+/**
+ * \brief
+ */
+void mux_print_decoded(void);
 
 #endif /* MUX_H */
