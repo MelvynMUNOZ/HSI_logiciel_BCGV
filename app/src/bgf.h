4705de36f266be505aabd3b72633ffe647dee467
Raphael CAUSSE <raphael.causse2@gmail.com>
2025-01-11 21:18:12 +0100

    update: bgf
    Refactor to use new type bgf_msg_t for internal usage
    Update coding style + comments

--- app/src/bgf.h
+++ app/src/bgf.h
@@ -1,30 +1,31 @@
 /**
- * \file        bgf.h
- * \author      Melvyn MUNOZ
- * \date        2025-01-06
- * \brief       This is the code to encode the message that BCGV send do BGF.
+ * \file bgf.h
+ * \brief Interface of BGF system.
+ * \details Read and write serial frames of BGF from driver.
+ * \author Melvyn MUNOZ
  */
 
 #ifndef BGF_H
 #define BGF_H
 
-#include <stdint.h>
+/***** Includes **************************************************************/
+
 #include "bcgv_api.h"
 
-/**
- * \brief Read the serial data of the driver, set the bit ack if there are differents values
- *
- * \param drvFd : Fd driver
- * \return boolean : the execution of the function is correct
- */
-bool bgf_read_serial_message(int32_t drvFd);
+/***** Functions *************************************************************/
 
 /**
- * \brief Check if a flag is different that the precedent flag, in this case, write a message to bgf
- *
- * \param drvFd : Fd driver
- * \return boolean : the execution of the function is correct
+ * \brief Read all available serial frames for BGF.
+ * \param drv_fd : Driver file descriptor.
+ * \return int32_t : Number of serial read, or -1 if an error occured
  */
-bool bgf_write_serial_message(int32_t drvFd);
+int32_t bgf_read_frames(int32_t drv_fd);
+
+/**
+ * \brief Write all necessary messages as serial frames.
+ * \param drv_fd : Driver file descriptor.
+ * \return int32_t : Number of errors while writing
+ */
+int32_t bgf_write_frames(int32_t drv_fd);
 
 #endif /* BGF_H */
